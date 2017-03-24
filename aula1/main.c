dce683cec83a2be6322b8091bb9c7360a5e680c3
paulosebastiao <paulosebastiao@usp.br>
2017-03-24 12:19:11 -0300

    Add files via upload

---
+++ aula1/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+int main()
+
+{
+    int x=0;
+    int h=0;
+    int m=0;
+    int s=0;
+    int dh=0;
+    int dm=0;
+    int ds=0;
+ printf("digite a hora");
+    scanf("%dh", &dh);
+ printf("digite o minuto");
+    scanf("%dm", &dm);
+ printf("digite o segundo");
+    scanf("%ds", &ds);
+loop:
+    Sleep(1000);
+    if (s==60) {s=0; m++;}
+    if (m==60) {h++; m=0;}
+    if (h==24) {h=0;}
+    printf("\r         hora:  %2dh   %2dm  %2ds", h, m, s);
+    s++;
+        if (h==dh && m==dm && s==ds){
+     while (x<10) {
+            printf("\n acorda!!!!");}
+    }
+    goto loop;
+    return 0;
+}
